8af1dc08bddb9b40d6074dad6770ff275c49eade
Sergei Solodzhuk <solodzh@gmail.com>
2023-02-19 05:54:14 +0200

    3

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import { render } from 'react-dom';
 import App from './app/App';
 
-import './shared/config/i18n/i18n';
-import { ErrorBoundary } from './app/providers/ErrorBoundary';
-
 render(
     <App />,
     document.getElementById('root'),
